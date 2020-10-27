5e2c76734aeac25448dded7e4e90a5508b06b098
Brent Johnson <brentcharlesjohnson@gmail.com>
2020-10-27 22:56:36 +0100

    adds type definitions for supertest

--- dist/__tests__/sample.spec.js
+++ dist/__tests__/sample.spec.js
@@ -7,15 +7,17 @@ const server_1 = __importDefault(require("../server"));
 const supertest_1 = __importDefault(require("supertest"));
 const sequelize_1 = require("../sequelize");
 const request = supertest_1.default(server_1.default);
-/* tests could be improved by mocking the database connection
+/*
+ * tests could be improved by mocking the database connection
  * tests are brittle because they are not isolated
  */
 describe('CRUD operations for Note', () => {
     beforeAll(async () => {
         await sequelize_1.sequelize.sync();
     });
-    afterAll(async () => {
+    afterAll(async (done) => {
         sequelize_1.sequelize.close();
+        return done;
     });
     it('Should create a new note', async () => {
         const response = await request
@@ -24,18 +26,18 @@ describe('CRUD operations for Note', () => {
             title: "Testing 1, 2, 3",
             message: "Lorem ipsum"
         });
-        expect(response.statusCode).toBe(201);
+        expect(response.status).toBe(201);
         expect(response.body).toHaveProperty('note');
     });
     it('Should get a note', async () => {
         const response = await request
             .get('/api/notes/1');
-        expect(response.statusCode).toBe(200);
+        expect(response.status).toBe(200);
     });
     it('Should list all notes', async () => {
         const response = await request
             .get('/api/notes');
-        expect(response.statusCode).toBe(200);
+        expect(response.status).toBe(200);
         expect(response.body).toHaveLength(1);
     });
     it('Should update a note', async () => {
@@ -44,7 +46,7 @@ describe('CRUD operations for Note', () => {
             .send({
             title: "Test updated"
         });
-        expect(response.statusCode).toBe(200);
+        expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('note');
         expect(response.body.note).toHaveProperty('title', 'Test updated');
     });
@@ -54,17 +56,17 @@ describe('CRUD operations for Note', () => {
             .send({
             message: null
         });
-        expect(response.statusCode).toBe(400);
+        expect(response.status).toBe(400);
         expect(response.body).toHaveProperty('errors');
     });
     it('Should delete a note', async () => {
         const response = await request
             .delete('/api/notes?id=1');
-        expect(response.statusCode).toBe(200);
+        expect(response.status).toBe(200);
     });
     it('Should respond with status code 404 if resource is not found.', async () => {
         const response = await request
             .get('/api/notes/1');
-        expect(response.statusCode).toBe(404);
+        expect(response.status).toBe(404);
     });
 });
--- src/__tests__/sample.spec.ts
+++ src/__tests__/sample.spec.ts
@@ -27,20 +27,20 @@ describe('CRUD operations for Note', () => {
                 title: "Testing 1, 2, 3",
                 message: "Lorem ipsum"
             });
-        expect(response.statusCode).toBe(201);
+        expect(response.status).toBe(201);
         expect(response.body).toHaveProperty('note');
     });
 
     it('Should get a note', async () => {
         const response = await request
             .get('/api/notes/1');
-        expect(response.statusCode).toBe(200);
+        expect(response.status).toBe(200);
     });
 
     it('Should list all notes', async () => {
         const response = await request
             .get('/api/notes');
-        expect(response.statusCode).toBe(200);
+        expect(response.status).toBe(200);
         expect(response.body).toHaveLength(1);
     });
 
@@ -51,7 +51,7 @@ describe('CRUD operations for Note', () => {
             .send({
                 title: "Test updated"
             });
-        expect(response.statusCode).toBe(200);
+        expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('note');
         expect(response.body.note).toHaveProperty('title', 'Test updated');
     });
@@ -62,19 +62,19 @@ describe('CRUD operations for Note', () => {
             .send({
                 message: null 
             });
-        expect(response.statusCode).toBe(400);
+        expect(response.status).toBe(400);
         expect(response.body).toHaveProperty('errors');
     });
 
     it('Should delete a note', async () => {
         const response = await request
             .delete('/api/notes?id=1');
-        expect(response.statusCode).toBe(200); 
+        expect(response.status).toBe(200); 
     });
 
     it('Should respond with status code 404 if resource is not found.', async () => {
         const response = await request
             .get('/api/notes/1');
-        expect(response.statusCode).toBe(404);
+        expect(response.status).toBe(404);
     })
 });
